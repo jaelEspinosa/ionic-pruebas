71781ce7c00ff8e9bdeb3a9350819c075d2462c0
jaelEspinosa <espinosa.lucia@bootcamp-upgrade.com>
2023-10-04 00:33:03 +0200

    searchbar

--- src/app/customers/customers.page.ts
+++ src/app/customers/customers.page.ts
@@ -16,21 +16,32 @@ private router = inject( Router )
 private customersService = inject ( CustomersService )
 
 public customers : User[] = []
-
+public searchedUser! :User[]
 
 ngOnInit() {
     this.getAllCustomers()
+
   }
 
 
-  getAllCustomers () {
+getAllCustomers () {
   this.customersService.getCustomers()
     .subscribe ({
       next: res => {
         this.customers = res
-
+        this.searchedUser = this.customers
       }
     })
+}
+
+searchCustomer(event:any){
+  const text = event.target.value;
+  this.searchedUser = this.customers;
+  if (text && text.trim() !== ''){
+    this.searchedUser = this.searchedUser.filter((user:User)=>{
+      return (user.name.toLowerCase().includes(text))
+    })
+  }
 }
 
 goToHome(){
